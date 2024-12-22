ea1e6bf7f24f6fe61beca5cfefbfb68ee3744801
letienndat <attn110601@gmail.com>
2024-12-22 18:18:52 +0700

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Lê Tiến Đạt on 22/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
